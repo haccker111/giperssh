abe4e99490ca648a2cdb4086158132a1ea625a61
haccker111 <m1119911894@gmail.com>
2021-02-17 15:16:23 +0700

    Init

--- sshangl.sh
+++ sshangl.sh
@@ -1,17 +1,28 @@
-clear 
+clear
 echo "                     GIPER SSH "
-echo "Change password (1), Start ssh server on port 8022 (2)"
+echo "Change password (1), Start ssh server on port 8022 (2) , Find out login to connect (3)"
 read numb
 if [ $numb = "1" ]
 then
-passwd 
+passwd
 else
+if [ $numb = "2" ]
+then
 echo -e "\e[32mHello! Do you want to continue?\e[0m";
 read -p 'y' userinput1;
 #
 sshd
-echo -e "\e[33mIf you want to stop ssh tunnel then press enter\e[0m";
+echo -e "\e[33mIf you want to stop the ssh tunnel then press enter"
 read -p 'stop' userinput3;
 #
 pkill  sshd
+else
+if [ $numb = "3" ]
+then
+echo "Here is your login:"
+whoami
+else
+echo "Error"
+fi
+fi
 fi
